5a512f3c79872e1caa3a243d5a689eab2a44346b
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-11-28 01:28:54 +0600

    mongo setup

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xasmzpl.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run() {
+  try {
+    const collection = client.db("autoStyx").collection("users");
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", async (req, res) => {
   res.send("AutoStyx server is running");
